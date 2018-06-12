9ad144f5ddd05e91c30f8937a01918a2ed53a0c2
Haram Yang <haryan@umich.edu>
2018-06-12 12:06:28 -0400

    displaying class info on right side

--- prev_info.js
+++ prev_info.js
@@ -1,38 +1,74 @@
 
-/*
+
 var class_list = {};
 
-function Class(classname, professorname, grade) {
+function Class(classname, professorname, grade, workload, difficulty) {
     this.className = classname;
     this.professorName = professorname;
     this.classGrade = grade;
+    this.workLoad = workload;
+    this.diff = difficulty;
 }
-*/
+
 
 window.onload = function() {
 
 	var submit = document.getElementById('class-form');
 
 	submit.onsubmit = function(z) {
+
 		z.preventDefault();
+
+		// store input values
 		var a = document.getElementById("class_name").value;
 		var b = document.getElementById("professor_name").value;
 		var c = document.getElementById("grade").value;
 		var d = document.getElementById("work_load").value;
 		var e = document.getElementById("difficulty").value;
 
-		document.getElementById("className").innerHTML = a;
-		document.getElementById("professorName").innerHTML = b;
-		document.getElementById("outputGrade").innerHTML = c;
-		document.getElementById("workLoad").innerHTML = d;
-		document.getElementById("outputDiff").innerHTML = e;
+		// reset the input forms
+		document.getElementById("class_name").value = "";
+		document.getElementById("professor_name").value = "";
+		document.getElementById("grade").value = "A+";
+		document.getElementById("work_load").value = "";
+		document.getElementById("difficulty").value = "";
 
-		//console.log(submit.difficulty.value);
+		var number = 1;
+		var pnl = document.getElementById("output_class_name");
+        var dynDiv = document.createElement("div");
+        var myP = document.createElement("P");
+		dynDiv.id = "class" + number;
+        number = number + 1;
+
+        var a_t = document.createTextNode("Class name: " + a);
+        var b_t = document.createTextNode(" Professor name: " + b);
+        var c_t = document.createTextNode(" Grade: " + c);
+        var d_t = document.createTextNode(" Workload: " + d);
+        var e_t = document.createTextNode(" Difficulty: " + e);
+
+        myP.appendChild(a_t);
+        myP.appendChild(b_t);
+        myP.appendChild(c_t);
+        myP.appendChild(d_t);
+        myP.appendChild(e_t);
+
+        dynDiv.appendChild(myP);
+        //dynDiv.innerHTML = "Class name: " + a + myBr + " Professor name: " + b + myBr + " Grade: " + c + myBr + " Workload: " + d + myBr + " Difficulty: " + e;
+        pnl.appendChild(dynDiv);
+
 
 		/*
-		var class1 = Class(a, b, c);
+		document.getElementById("className").innerHTML = "Class name: " + a;
+		document.getElementById("professorName").innerHTML = "Professor name: " + b;
+		document.getElementById("outputGrade").innerHTML = "Grade: " + c;
+		document.getElementById("workLoad").innerHTML = "Workload: " + d;
+		document.getElementById("outputDiff").innerHTML = "Difficulty: " + e;
+
+		
+		var class1 = Class(a, b, c, d, e);
 
 		class_list[0] = class1;
 		*/
+		
 	};
 }
