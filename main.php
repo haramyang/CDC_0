e4667050b6fa74736596468f9e9ae60aa053cd6e
Jhuree Hong <jhuree@Jhurees-MacBook-Pro.local>
2018-06-12 12:04:37 -0400

    just so i dont get conflicts later

--- main.php
+++ main.php
@@ -2,7 +2,13 @@
 <html>
 	<body>
 
-	<h1>My first PHP page</h1>
+	<h1>TEST</h1>
+
+	Welcome <?php
+		echo $GET["uname"];
+	?>
+	<br>
+
 
 	<?php
 
@@ -15,8 +21,25 @@
 	}
 
 	$test = new Car();
+
 	echo "Hello World!";
 	echo $test->model;
+	echo count($cars);
+
+	foreach ($cars as $value) {
+		echo "$value <br>"
+	}
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// collect value of input field
+		$name = $_REQUEST['psw'];
+		if (empty($name)) {
+			echo "No password";
+		}
+		else {
+			echo $name;
+		}
+	}
 
 	?>
 
