d6f4d982f22b3ed265f873f6c71b6f78077e9eae
Haram Yang <haryan@umich.edu>
2018-06-25 22:44:02 -0400

    changed fonts on previnfo

--- js/previnfo.js
+++ js/previnfo.js
@@ -57,7 +57,7 @@ window.onload = function() {
         //dynDiv.innerHTML = "Class name: " + a + myBr + " Professor name: " + b + myBr + " Grade: " + c + myBr + " Workload: " + d + myBr + " Difficulty: " + e;
         pnl.appendChild(dynDiv);
 
-        document.getElementById("output_class_name").style.font = "avenir";
+        document.getElementById("output_class_name").style.fontFamily = "avenir";
 
 
 		/*
